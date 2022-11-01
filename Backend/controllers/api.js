d3d9073dd690959783430123c55147ccd9c3420b
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-11-01 16:15:24 +0300

    creating controller function for regular evants

--- Backend/controllers/api.js
+++ Backend/controllers/api.js
@@ -40,7 +40,54 @@ const loginUser = (req, res) => {
     })
 };
 
+// regular events function
+const regularEvents =(req, res) => {
+
+    let events = [
+        {
+            "_id": "1",
+            "name": "Angular",
+            "description": "Lorem Ipsum",
+            "date": "2022-11-1T15:55:43.511Z"
+        },
+        {
+            "_id": "2",
+            "name": "Mongodb",
+            "description": "Lorem Ipsum",
+            "date": "2022-11-1T15:55:43.511Z"
+        },
+        {
+            "_id": "3",
+            "name": "Express",
+            "description": "Lorem Ipsum",
+            "date": "2022-11-1T15:55:43.511Z"
+        },
+        {
+            "_id": "4",
+            "name": "Mongoose",
+            "description": "Lorem Ipsum",
+            "date": "2022-11-1T15:55:43.511Z"
+        },
+        {
+            "_id": "5",
+            "name": "CORS",
+            "description": "Lorem Ipsum",
+            "date": "2022-11-1T15:55:43.511Z"
+        },
+        {
+            "_id": "6",
+            "name": "Bootstrap",
+            "description": "Lorem Ipsum",
+            "date": "2022-11-1T15:55:43.511Z"
+        }
+    ];
+
+    res.json(events)
+
+}
+
 module.exports = {
     registerUser,
     loginUser,
+    regularEvents,
 }
--- Backend/routes/api.js
+++ Backend/routes/api.js
@@ -27,50 +27,7 @@ router.post('/register');
 router.post('/login');
 
 // regular events endpoint
-router.get('/events/regular', (req, res) => {
-
-    let events = [
-        {
-            "_id": "1",
-            "name": "Angular",
-            "description": "Lorem Ipsum",
-            "date": "2022-11-1T15:55:43.511Z"
-        },
-        {
-            "_id": "2",
-            "name": "Mongodb",
-            "description": "Lorem Ipsum",
-            "date": "2022-11-1T15:55:43.511Z"
-        },
-        {
-            "_id": "3",
-            "name": "Express",
-            "description": "Lorem Ipsum",
-            "date": "2022-11-1T15:55:43.511Z"
-        },
-        {
-            "_id": "4",
-            "name": "Mongoose",
-            "description": "Lorem Ipsum",
-            "date": "2022-11-1T15:55:43.511Z"
-        },
-        {
-            "_id": "5",
-            "name": "CORS",
-            "description": "Lorem Ipsum",
-            "date": "2022-11-1T15:55:43.511Z"
-        },
-        {
-            "_id": "6",
-            "name": "Bootstrap",
-            "description": "Lorem Ipsum",
-            "date": "2022-11-1T15:55:43.511Z"
-        }
-    ];
-
-    res.json(events)
-
-});
+router.get('/events/regular');
 
 // Special events endpoint
 router.get('/events/special', (req, res) => {
