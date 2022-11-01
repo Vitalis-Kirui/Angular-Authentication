62eb7dff633b7e721a3cfa3d75acb09c4c024073
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-11-01 15:02:45 +0300

    creating api route for registering users

--- Backend/routes/api.js
+++ Backend/routes/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const User = require('../models/user');
 
 // connecting to database
 const connectionString = 'The string will come here.';
@@ -17,6 +18,21 @@ mongoose.connect(connectionString)
 // Api routes home page
 router.get('/', (req, res) => {
     res.send('This is from API routes')
+});
+
+// Registration api
+router.get('/register', (req, res) => {
+    let userData = req.body;
+    let user  = new User(userData);
+
+    user.save((error, registeredUser) => {
+        if(error){
+            console.log(error);
+        }else{
+            res.send(registeredUser);
+        }
+    });
+
 });
 
 module.exports = router;
