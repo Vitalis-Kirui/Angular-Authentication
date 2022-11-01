4a52ba58c6496633e3c227ee293f0671720e4449
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-11-01 14:47:45 +0300

    connecting to mongoDB database

--- Backend/routes/api.js
+++ Backend/routes/api.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+// connecting to database
+const connectionString = 'The string will come here.';
+
+mongoose.connect(connectionString)
+    .then((results) =>{
+        console.log('Successfull connection to database')
+    })
+    .catch((error) =>{
+        console.log(error);
+    });
+
 
 // Api routes home page
 router.get('/', (req, res) => {
