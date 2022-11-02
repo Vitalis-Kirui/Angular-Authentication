522b01ef8c402a1316c8cdd99d2056f317859e77
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-11-02 15:58:39 +0300

    fetching regular events in events service

--- Frontend/Auth-Frontend/src/app/Services/events.service.ts
+++ Frontend/Auth-Frontend/src/app/Services/events.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,14 @@ import { Injectable } from '@angular/core';
 })
 export class EventsService {
 
-  constructor() { }
+  // Regular events url
+  private regularUrl = 'http://localhost:3000/api/events/regular';
+
+  constructor(private http  : HttpClient) { }
+
+  // Fetching regular events
+  regularEvents(events : any){
+    return this.http.get<any>(this.regularUrl , events);
+  };
+
 }
