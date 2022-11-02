10c16342e5d3ab16acee35a860f4e7c9cbaa59ad
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-11-02 14:55:54 +0300

    creating registration functionality

--- Frontend/Auth-Frontend/src/app/Services/authentication.service.ts
+++ Frontend/Auth-Frontend/src/app/Services/authentication.service.ts
@@ -6,5 +6,14 @@ import { Injectable } from '@angular/core';
 })
 export class AuthenticationService {
 
+  // Register url
+  private registerUrl = 'http://localhost:3000/api/register';
+
   constructor(private http : HttpClient) { }
+
+  // Register user function
+  registerUser(user : any){
+    return this.http.post(this.registerUrl , user);
+  }
+
 }
