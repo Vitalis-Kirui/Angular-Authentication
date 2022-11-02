7ba22afc3ffe2a2fe1d5694c6de13f1d94d3e50c
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-11-02 15:23:29 +0300

    using ng model for fetch data from login form

--- Frontend/Auth-Frontend/src/app/Components/login/login.component.ts
+++ Frontend/Auth-Frontend/src/app/Components/login/login.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  // User details
+  userLoginData: any = {};
+
   constructor() { }
 
   ngOnInit(): void {
