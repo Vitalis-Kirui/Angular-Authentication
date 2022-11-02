da87ae179f3c81e7f6fe34f949c2bdf0ec496347
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-11-02 15:31:11 +0300

    creating a login function in authentication service

--- Frontend/Auth-Frontend/src/app/Services/authentication.service.ts
+++ Frontend/Auth-Frontend/src/app/Services/authentication.service.ts
@@ -9,11 +9,20 @@ export class AuthenticationService {
   // Register url
   private registerUrl = 'http://localhost:3000/api/register';
 
+  // Logine url
+  private loginUrl = 'http://localhost:3000/api/login';
+
+
   constructor(private http : HttpClient) { }
 
   // Register user function
   registerUser(user : any){
     return this.http.post(this.registerUrl , user);
   }
+
+  // Login user function
+  loginUser(user : any){
+    return this.http.post(this.loginUrl, user);
+  };
 
 }
