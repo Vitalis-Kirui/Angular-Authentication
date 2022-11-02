2106b87f31acfed148b888c56cc26d657104d42e
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-11-02 16:13:07 +0300

    fetching special events in events services

--- Frontend/Auth-Frontend/src/app/Services/events.service.ts
+++ Frontend/Auth-Frontend/src/app/Services/events.service.ts
@@ -9,11 +9,19 @@ export class EventsService {
   // Regular events url
   private regularUrl = 'http://localhost:3000/api/events/regular';
 
+    // Special events url
+    private specialUrl = 'http://localhost:3000/api/events/special';
+
   constructor(private http  : HttpClient) { }
 
   // Fetching regular events
   regularEvents(){
     return this.http.get<any>(this.regularUrl);
+  };
+
+  // Fetching special events
+  specialEvents(){
+    return this.http.get<any>(this.specialUrl);
   };
 
 }
