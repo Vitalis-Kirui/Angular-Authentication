eb4797a38e91a51ba79b10de35160875c01826ec
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-11-03 18:46:25 +0300

    creating logout function

--- Frontend/Auth-Frontend/src/app/Services/authentication.service.ts
+++ Frontend/Auth-Frontend/src/app/Services/authentication.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,7 @@ export class AuthenticationService {
   private loginUrl = 'http://localhost:3000/api/login';
 
 
-  constructor(private http : HttpClient) { }
+  constructor(private http : HttpClient, private router : Router) { }
 
   // Register user function
   registerUser(user : any){
@@ -23,6 +24,12 @@ export class AuthenticationService {
   // Login user function
   loginUser(user : any){
     return this.http.post<any>(this.loginUrl, user);
+  };
+
+  // Logging out users
+  logoutUser(){
+    localStorage.removeItem('token');
+    this.router.navigate(['/events/regular']);
   };
 
   // Logged in function for auth guard
