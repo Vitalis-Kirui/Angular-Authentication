74798bf0deed965b6c8eb6a2747f8c0c313a1c7c
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-11-03 19:31:50 +0300

    checking wether email already exist before registration

--- Backend/controllers/api.js
+++ Backend/controllers/api.js
@@ -4,17 +4,30 @@ const User = require('../models/user');
 // user registration
 const registerUser = (req, res) => {
     let userData = req.body;
-    let user  = new User(userData);
 
-    user.save((error, registeredUser) => {
+    User.findOne({email : userData.email}, (error, user) => {
+
         if(error){
             console.log(error);
-        }else{
-            let payload ={subject : registeredUser._id}
-            let token = jwt.sign(payload , 'secretkey')
-            res.send({token});
         }
-    });
+        if(user){
+            res.status(201).send("User with same email already registered");
+        }
+        else{
+            let user  = new User(userData);
+
+            user.save((error, registeredUser) => {
+                if(error){
+                    console.log(error);
+                }else{
+                    let payload ={subject : registeredUser._id}
+                    let token = jwt.sign(payload , 'secretkey')
+                    res.send({token});
+                }
+            });
+        }
+
+    })
 
 };
 
