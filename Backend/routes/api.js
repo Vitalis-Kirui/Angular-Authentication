2acebf31e8a5c455f48c6168270b401e000dbcd7
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-11-03 18:01:23 +0300

    creating a middleware to verify token

--- Backend/routes/api.js
+++ Backend/routes/api.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 const apiControllers = require('../controllers/api');
 
@@ -18,5 +19,33 @@ router.get('/events/regular', apiControllers.regularEvents);
 
 // Special events endpoint
 router.get('/events/special', apiControllers.specialEvents);
+
+// Verify token middleware
+function verifyToken(req, res, next){
+
+    // Checking authorization key in the headers
+    if(!req.headers.authorization){
+        res.status(401).send('Unauthorized request');
+    };
+
+    // If there is an authorization key
+    let token = req.headers.authorization.split(' ')[1];
+    if(token === 'null'){
+        res.status(401).send('Unauthorized request');
+    }
+
+    // Verify the token if not null
+    let payload = jwt.verify(token, 'secretkey')
+
+    // if token is not correct
+    if(!payload){
+        res.status(401).send('Unauthorized request');
+    };
+
+    req.userId = payload.subject
+
+    next();
+
+};
 
 module.exports = router;
