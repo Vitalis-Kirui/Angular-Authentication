28a758846875f1175eab673fb8b908f5cb87c2f0
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-11-03 17:09:23 +0300

    using authentication service to intercept real token

--- Frontend/Auth-Frontend/src/app/Services/token-interceptor.service.ts
+++ Frontend/Auth-Frontend/src/app/Services/token-interceptor.service.ts
@@ -1,18 +1,20 @@
 import { HttpInterceptor } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
+import { AuthenticationService } from './authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenInterceptorService implements HttpInterceptor {
 
-  constructor() { }
+  constructor(private service  : AuthenticationService, private injector : Injector) { }
 
   // Intercept method
   intercept(req : any , next : any){
+    let service2 = this.injector.get(AuthenticationService)
     let tokenizedReq = req.clone({
       setHeaders : {
-        Authorization : 'Bearer  xx.yy.zz'
+        Authorization : `Bearer ${service2.getToken()}`
       }
     });
 
