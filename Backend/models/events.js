27763e5e95db6fb6a837f002cb92451d57eefd3a
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-11-03 21:13:20 +0300

    creating an event model

---
+++ Backend/models/events.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const eventSchema = new Schema({
+
+    name : {
+        type : String,
+        require : true
+    },
+    description : {
+        type : String,
+        require : true
+    }
+
+},
+{
+    timestamps : true
+}
+);
+
+module.exports = mongoose.model('Event', eventSchema , 'events')
